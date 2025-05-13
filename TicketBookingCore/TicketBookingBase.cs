1e3884cba3c7da4f99e94fe6fac3589a141f523b
Gene Suelo <gene.suelo@gmail.com>
2025-05-13 15:05:58 +0200

    Steg 3.1: Lägg till default initializers till TicketBookingBase

--- TicketBookingCore/TicketBookingBase.cs
+++ TicketBookingCore/TicketBookingBase.cs
@@ -8,8 +8,9 @@ namespace TicketBookingCore
 {
     public class TicketBookingBase
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
+        // Förhindra CS8618: garantera icke-null genom att som standard använda en tom sträng
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
